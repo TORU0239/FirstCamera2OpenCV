fdafa744e05e73872747520248b000353836662e
Toru Choi <toru.choi@11street.my>
2018-10-10 18:16:01 +0800

    initial camera view by OpenCV

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,7 @@
 #include <jni.h>
 #include <string>
+//#include <opencv2/opencv.hpp>
+//using namespace cv;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_my_com_toru_firstcamera2opencv_MainActivity_stringFromJNI(
@@ -11,7 +13,29 @@ Java_my_com_toru_firstcamera2opencv_MainActivity_stringFromJNI(
 
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_my_com_toru_firstcamera2opencv_ui_MainActivity_stringFromJNI(JNIEnv *env, jobject instance) {
+Java_my_com_toru_firstcamera2opencv_ui_MainActivity_stringFromJNI(JNIEnv *env,
+                                                                  jobject) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_my_com_toru_firstcamera2opencv_ui_MainActivity_testFromJNI(JNIEnv *env,
+                                                                jobject) {
+    std::string testHello = "This is written by Toru";
+
+    return env->NewStringUTF(testHello.c_str());
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_my_com_toru_firstcamera2opencv_ui_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject instance,
+                                                                     jlong matAddrInput,
+                                                                     jlong matAddrResult) {
+
+    // 입력 RGBA 이미지를 GRAY 이미지로 변환
+//    Mat &matInput = *(Mat *)matAddrInput;
+//    Mat &matResult = *(Mat *)matAddrResult;
+//    cvtColor(matInput, matResult, CV_RGBA2GRAY);
 }
--- app/src/main/java/my/com/toru/firstcamera2opencv/ui/MainActivity.java
+++ app/src/main/java/my/com/toru/firstcamera2opencv/ui/MainActivity.java
@@ -1,38 +1,116 @@
 package my.com.toru.firstcamera2opencv.ui;
 
 import android.annotation.TargetApi;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import my.com.toru.firstcamera2opencv.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener, CameraBridgeViewBase.CvCameraViewListener2 {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int PERMISSIONS_REQUEST_CODE = 1000;
 
     private String[] PERMISSIONS  = {"android.permission.CAMERA"};
 
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private Mat matInput;
+    private Mat matResult;
+
+    private Mat mRgba;
+    private Mat mRgbaF;
+    private Mat mRgbaT;
+
+//    public native void ConvertRGBtoGray(long matAddrInput, long matAddrResult);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
-        Log.w(TAG, "string from JNI:: " + stringFromJNI());
+
+        mOpenCvCameraView = findViewById(R.id.activity_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+        mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            //퍼미션 상태 확인
             if (!hasPermissions(PERMISSIONS)) {
-                //퍼미션 허가 안되어있다면 사용자에게 요청
                 requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            }
+            else{
+                mOpenCvCameraView.enableView();
             }
         }
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "onResume :: Internal OpenCV library not found.");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
+        }
+        else {
+            Log.d(TAG, "onResum :: OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    public void onDestroy() {
+        if (mOpenCvCameraView != null){
+            mOpenCvCameraView.disableView();
+        }
+        super.onDestroy();
+    }
+
+
+    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    mOpenCvCameraView.enableView();
+                    break;
+
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+
 
     //region Camera Permission
     private boolean hasPermissions(String[] permissions) {
@@ -66,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
     @TargetApi(Build.VERSION_CODES.M)
     private void showDialogForPermission(String msg) {
         AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this);
-        builder.setTitle("알림");
+        builder.setTitle("NOTICE!!");
         builder.setMessage(msg);
         builder.setCancelable(false);
         builder.setPositiveButton("YES", (dialog, id) -> requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE));
@@ -75,12 +153,34 @@ public class MainActivity extends AppCompatActivity {
     }
     //endregion
 
-    //region JNI handling
-    public native String stringFromJNI();
-
     static {
-        System.loadLibrary("native-lib");
         System.loadLibrary("opencv_java3");
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaT = new Mat(width, width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+//        Core.transpose(mRgba, mRgbaT);
+//        Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(), 0,0, 0);
+//        Core.flip(mRgbaF, mRgba, 1 );
+        return mRgba; // This function must return
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
     }
     //endregion
 }
