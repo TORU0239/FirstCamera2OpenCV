b0e1565d83987438bc5778ad44d95deac0e0acd5
Toru Choi <toru.choi@11street.my>
2018-10-12 17:07:13 +0800

    wip, for implementing callback interfaces

--- app/src/main/java/my/com/toru/firstcamera2opencv/ui/camera/CameraActivity.java
+++ app/src/main/java/my/com/toru/firstcamera2opencv/ui/camera/CameraActivity.java
@@ -1,14 +1,20 @@
 package my.com.toru.firstcamera2opencv.ui.camera;
 
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CaptureFailure;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
 import android.media.ImageReader;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Size;
+import android.view.Surface;
 import android.view.TextureView;
 
 import my.com.toru.firstcamera2opencv.R;
@@ -18,18 +24,74 @@ public class CameraActivity extends AppCompatActivity {
 
     //region textureView
     private TextureView textureView;
-    private TextureView.SurfaceTextureListener surfaceTextureListener;
+    private TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            openCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {}
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {}
+    };
     // endregion
 
     //region CameraDevice
     private CameraDevice cameraDevice;
-    private CameraDevice.StateCallback cameraStateCallback;
+    private CameraDevice.StateCallback cameraStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            cameraDevice = camera;
+            createCameraPreview();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {}
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {}
+    };
     //endregion
 
     //region Capture
     private CameraCaptureSession captureSession;
-    private CameraCaptureSession.CaptureCallback captureCallback;
-    private CameraCaptureSession.StateCallback captureStateCallback;
+    private CameraCaptureSession.CaptureCallback captureCallback = new CameraCaptureSession.CaptureCallback() {
+        @Override
+        public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
+            super.onCaptureStarted(session, request, timestamp, frameNumber);
+        }
+
+        @Override
+        public void onCaptureProgressed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureResult partialResult) {
+            super.onCaptureProgressed(session, request, partialResult);
+        }
+
+        @Override
+        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+            super.onCaptureCompleted(session, request, result);
+        }
+
+        @Override
+        public void onCaptureFailed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureFailure failure) {
+            super.onCaptureFailed(session, request, failure);
+        }
+    };
+
+    private CameraCaptureSession.StateCallback captureStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(@NonNull CameraCaptureSession session) {}
+
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession session) {}
+    };
+
     private CaptureRequest captureRequest;
     private CaptureRequest.Builder captureRequestBuilder;
     //endregion
@@ -37,14 +99,16 @@ public class CameraActivity extends AppCompatActivity {
     //region Image and Size
     private Size size;
     private ImageReader imageReader;
-    private ImageReader.OnImageAvailableListener imageAvailableListener;
+    private ImageReader.OnImageAvailableListener imageAvailableListener = new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader reader) {}
+    };
     //endregion
 
     //region in charge of Background Task
     private Handler backgroundHander;
     private HandlerThread backgroundThread;
     // endregion
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,10 +121,18 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        initializeBackgroundThread();
+        if (textureView.isAvailable()) {
+            openCamera();
+        }
+        else {
+            textureView.setSurfaceTextureListener(surfaceTextureListener);
+        }
     }
 
     @Override
     protected void onPause() {
+        stopBackgroundThread();
         super.onPause();
     }
 
@@ -69,9 +141,33 @@ public class CameraActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
+    //region Camera Related Work
     private void initCamera(){
         textureView = findViewById(R.id.camera_texture);
+        textureView.setSurfaceTextureListener(surfaceTextureListener);
     }
+
+    private void openCamera(){
+        //TODO: initializing camera, and configuring camera status callback.
+    }
+
+    private void closeCamera(){
+        if(cameraDevice != null){
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    }
+    //endregion
+
+    //region CameraPreview
+    private void createCameraPreview(){
+
+    }
+
+    private void updateCameraPreview(){
+
+    }
+    //endregion
 
     //region Background Work
     private void initializeBackgroundThread(){
@@ -90,5 +186,5 @@ public class CameraActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-    //endofregion
+    //endregion
 }
